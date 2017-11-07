8a2097562de28ce589f7e32e5eaaf34e49bfe71e
yngadeum <yanncedrik@gmail.com>
2017-11-07 10:44:01 -0500

    First push

---
+++ index.js
@@ -0,0 +1,53 @@
+//Linear Grandient Image by YannCed ☺☻♥
+
+import React, { Component } from 'react'
+import LinearGradient from 'react-native-linear-gradient'
+
+import {
+    View,
+    Image,
+    StyleSheet,
+} from 'react-native'
+
+export default class ImageGradient extends Component {
+
+    constructor() {
+        super();
+
+    }
+
+    render() {
+        const { 
+            mainStyle = styles.main, 
+            gradientStyle = styles.gradientStyle ,
+            imageUrl = 'http://www.acmetools.com/wcsstore/AuroraStorefrontAssetStore/images/generic-error-icon-lrg.png' ,
+            startPosition = { x: 0.0, y: 0.50 },
+            rgbcsvStart = '255,255,255',
+            rgbcsvEnd = '0,0,0',
+            localImage = false,
+        } = this.props
+        return (
+            <Image style={mainStyle} source={ localImage ? require(imageUrl) : { uri: imageUrl } }>
+                <LinearGradient style={gradientStyle} start={startPosition} colors={[`rgba(${rgbcsvStart},0.0)`, `rgba(${rgbcsvEnd},0.9)`]} >
+                    {this.props.children}
+                </LinearGradient>
+            </Image>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    gradientStyle: {
+        flex: 1,
+        backgroundColor: 'rgba(166,188,204,0.0)',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    main: {
+        flex: 1,
+        height: '100%',
+        width: '100%',
+
+    },
+});
